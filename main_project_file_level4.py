6c7e5d82c30439b1d6129328aa57017f9336f95c
radhikakhatrii <74490667+radhikakhatrii@users.noreply.github.com>
2021-05-22 01:54:37 +0500

    Add files via upload

--- main_project_file_level4.py
+++ main_project_file_level4.py
@@ -38,7 +38,7 @@ def level_4():
         maze.shape('square')
         maze.color('#00008b')
         # the size helps us to fit the maze within the window frame
-        maze.shapesize(0.49,0.49,0.49)
+        maze.shapesize(0.49, 0.49, 0.49)
         maze.penup()  # this hides the working of turtle in background from the user
         maze.speed(0)  # hides the moving turtle across the screen
         # iterating over the rows and columns of our frame to visit each and every character
@@ -60,7 +60,7 @@ def level_4():
                     start = turtle.Turtle()
                     start.shape('square')
                     start.color('#b0e0e6')
-                    start.shapesize(0.49,0.49,0.49)
+                    start.shapesize(0.49, 0.49, 0.49)
                     start.penup()
                     start.speed(0)
                     start.goto(screen_x, screen_y)
@@ -71,7 +71,7 @@ def level_4():
                     end = turtle.Turtle()
                     end.shape('square')
                     end.color('#b0e0e6')
-                    end.shapesize(0.49,0.49,0.49)
+                    end.shapesize(0.49, 0.49, 0.49)
                     end.penup()
                     end.speed(0)
                     end.goto(screen_x, screen_y)
@@ -129,24 +129,25 @@ def level_4():
         back_track.shapesize(0.49, 0.49, 0.49)
         back_track.goto(x, y)
         back_track.stamp()
-        #the method below has a pre-requisite of running the BFS function first where we search for all pixels that can be visited in the pixels_visited dictionary
+        # the method below has a pre-requisite of running the BFS function first where we search for all pixels that can be visited in the pixels_visited dictionary
         # while (x, y) != (begin_x, begin_y):
         #     back_track.goto(pixels_visited[x, y])
         #     back_track.stamp()
         #     x, y = pixels_visited[x, y]
-        #we instead use this alternate method where we ony run BFS once and calculate all the pixels that are there in the shortest path so that when timer gets over, the user doesnt have to wait a long time of waiting for BFS to look what the shortest path was
+        # we instead use this alternate method where we ony run BFS once and calculate all the pixels that are there in the shortest path so that when timer gets over, the user doesnt have to wait a long time of waiting for BFS to look what the shortest path was
         # final_path=[]
         # while (x, y) != (begin_x, begin_y):
         #   x, y = pixels_visited[x, y]
         #   final_path.append((x,y))
         # print(final_path)
-        #using the solution above we print the final path in the terminal and then copy the solution into a final_path variable
-        final_path=[(294, -282), (284, -282), (274, -282), (264, -282), (254, -282), (244, -282), (234, -282), (224, -282), (214, -282), (204, -282), (194, -282), (184, -282), (174, -282), (164, -282), (164, -272), (164, -262), (164, -252), (164, -242), (164, -232), (164, -222), (154, -222), (144, -222), (144, -232), (144, -242), (144, -252), (144, -262), (144, -272), (134, -272), (124, -272), (114, -272), (104, -272), (104, -262), (104, -252), (104, -242), (104, -232), (104, -222), (94, -222), (84, -222), (84, -232), (84, -242), (84, -252), (84, -262), (84, -272), (74, -272), (64, -272), (54, -272), (44, -272), (34, -272), (24, -272), (14, -272), (4, -272), (-6, -272), (-16, -272), (-16, -262), (-16, -252), (-26, -252), (-36, -252), (-46, -252), (-56, -252), (-66, -252), (-76, -252), (-86, -252), (-96, -252), (-96, -242), (-96, -232), (-96, -222), (-96, -212), (-86, -212), (-76, -212), (-76, -202), (-76, -192), (-86, -192), (-96, -192), (-96, -182), (-96, -172), (-96, -162), (-96, -152), (-86, -152), (-76, -152), (-76, -142), (-76, -132), (-86, -132), (-96, -132), (-106, -132), (-106, -122), (-106, -112), (-106, -102), (-116, -102), (-126, -102), (-136, -102), (-136, -92), (-136, -82), (-136, -72), (-136, -62), (-136, -52), (-136, -42), 
-(-146, -42), (-156, -42), (-166, -42), (-176, -42), (-186, -42), (-186, -32), (-186, -22), (-186, -12), (-186, -2), (-186, 8), (-186, 18), (-176, 18), (-166, 18), (-166, 8), (-166, -2), (-166, -12), (-156, -12), (-146, -12), (-136, -12), (-126, -12), (-116, -12), (-106, -12), (-106, -22), (-106, -32), (-106, -42), (-96, -42), (-86, -42), (-76, -42), (-76, -52), (-76, -62), (-76, -72), (-66, -72), (-56, -72), (-46, -72), (-46, -82), (-46, -92), (-46, -102), (-46, -112), (-46, -122), (-36, -122), (-26, -122), (-16, -122), (-16, -112), (-16, -102), (-16, -92), (-6, -92), (4, -92), (14, -92), (14, -82), (14, -72), (4, -72), (-6, -72), (-16, -72), (-16, -62), (-16, -52), (-16, -42), (-26, -42), (-36, -42), (-46, -42), (-46, -32), (-46, -22), (-46, -12), (-56, -12), (-66, -12), (-76, -12), (-76, -2), (-76, 8), (-76, 18), (-86, 18), (-96, 18), (-106, 18), (-116, 18), (-126, 18), 
-(-136, 18), (-136, 28), (-136, 38), (-136, 48), (-136, 58), (-136, 68), (-136, 78), (-146, 78), (-156, 78), (-156, 88), (-156, 98), (-156, 108), (-156, 118), (-146, 118), (-136, 118), (-126, 118), (-116, 118), (-106, 118), (-106, 128), (-106, 138), (-116, 138), (-126, 138), (-136, 138), (-146, 138), (-156, 138), (-166, 138), (-166, 148), (-166, 158), (-166, 168), (-176, 168), (-186, 168), (-196, 168), (-206, 168), (-216, 168), (-216, 158), (-216, 148), (-216, 138), (-216, 128), (-216, 118), (-226, 118), (-236, 118), (-246, 118), (-256, 118), (-256, 128), (-256, 138), (-256, 148), (-256, 158), (-256, 168), (-256, 178), (-256, 188), (-256, 198), (-256, 208), (-256, 218), (-256, 228), (-266, 228), (-276, 228), (-286, 228), (-286, 238), (-286, 248), (-286, 258), (-286, 268), (-286, 278), (-286, 288), (-286, 298), (-296, 298)]
+        # using the solution above we print the final path in the terminal and then copy the solution into a final_path variable
+        final_path = [(294, -282), (284, -282), (274, -282), (264, -282), (254, -282), (244, -282), (234, -282), (224, -282), (214, -282), (204, -282), (194, -282), (184, -282), (174, -282), (164, -282), (164, -272), (164, -262), (164, -252), (164, -242), (164, -232), (164, -222), (154, -222), (144, -222), (144, -232), (144, -242), (144, -252), (144, -262), (144, -272), (134, -272), (124, -272), (114, -272), (104, -272), (104, -262), (104, -252), (104, -242), (104, -232), (104, -222), (94, -222), (84, -222), (84, -232), (84, -242), (84, -252), (84, -262), (84, -272), (74, -272), (64, -272), (54, -272), (44, -272), (34, -272), (24, -272), (14, -272), (4, -272), (-6, -272), (-16, -272), (-16, -262), (-16, -252), (-26, -252), (-36, -252), (-46, -252), (-56, -252), (-66, -252), (-76, -252), (-86, -252), (-96, -252), (-96, -242), (-96, -232), (-96, -222), (-96, -212), (-86, -212), (-76, -212), (-76, -202), (-76, -192), (-86, -192), (-96, -192), (-96, -182), (-96, -172), (-96, -162), (-96, -152), (-86, -152), (-76, -152), (-76, -142), (-76, -132), (-86, -132), (-96, -132), (-106, -132), (-106, -122), (-106, -112), (-106, -102), (-116, -102), (-126, -102), (-136, -102), (-136, -92), (-136, -82), (-136, -72), (-136, -62), (-136, -52), (-136, -42),
+                      (-146, -42), (-156, -42), (-166, -42), (-176, -42), (-186, -42), (-186, -32), (-186, -22), (-186, -12), (-186, -2), (-186, 8), (-186, 18), (-176, 18), (-166, 18), (-166, 8), (-166, -2), (-166, -12), (-156, -12), (-146, -12), (-136, -12), (-126, -12), (-116, -12), (-106, -12), (-106, -22), (-106, -32), (-106, -42), (-96, -42), (-86, -42), (-76, -42), (-76, -52), (-76, -62), (-76, -72), (-66, -72), (-56, -72), (-46, -72), (-46, -82), (-46, -
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                           92), (-46, -102), (-46, -112), (-46, -122), (-36, -122), (-26, -122), (-16, -122), (-16, -112), (-16, -102), (-16, -92), (-6, -92), (4, -92), (14, -92), (14, -82), (14, -72), (4, -72), (-6, -72), (-16, -72), (-16, -62), (-16, -52), (-16, -42), (-26, -42), (-36, -42), (-46, -42), (-46, -32), (-46, -22), (-46, -12), (-56, -12), (-66, -12), (-76, -12), (-76, -2), (-76, 8), (-76, 18), (-86, 18), (-96, 18), (-106, 18), (-116, 18), (-126, 18),
+                      (-136, 18), (-136, 28), (-136, 38), (-136, 48), (-136, 58), (-136, 68), (-136, 78), (-146, 78), (-156, 78), (-156, 88), (-156, 98), (-156, 108), (-156, 118), (-146, 118), (-136, 118), (-126, 118), (-116, 118), (-106, 118), (-106, 128), (-106, 138), (-116, 138), (-126, 138), (-136, 138), (-146, 138), (-156, 138), (-166, 138), (-166, 148), (-166, 158), (-166, 168), (-176, 168), (-186, 168), (-196, 168), (-206, 168), (-216, 168), (-216, 158), (-216, 148), (-216, 138), (-216, 128), (-216, 118), (-226, 118), (-236, 118), (-246, 118), (-256, 118), (-256, 128), (-256, 138), (-256, 148), (-256, 158), (-256, 168), (-256, 178), (-256, 188), (-256, 198), (-256, 208), (-256, 218), (-256, 228), (-266, 228), (-276, 228), (-286, 228), (-286, 238), (-286, 248), (-286, 258), (-286, 268), (-286, 278), (-286, 288), (-286, 298), (-296, 298)]
         for i in final_path:
-           back_track.goto(i)
-           back_track.stamp()
+            back_track.goto(i)
+            back_track.stamp()
     frame = ['+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++',
              's        +     +     +        +        +     +        +     +',
              '+        +     +     +        +        +     +        +     +',
@@ -208,25 +209,36 @@ def level_4():
              '+           +              +           +     +              +',
              '+           +              +           +     +              e',
              '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++']  # define a maze for our level
-    #we use this function to find the coordinates of the screen to start creating the maze from those coordinates so that it fits the screen size
-    # def buttonclick(x,y): 
+    # we use this function to find the coordinates of the screen to start creating the maze from those coordinates so that it fits the screen size
+    # def buttonclick(x,y):
     #     print("You clicked at this coordinate({0},{1})".format(x,y))
-  
+
     # #onscreen function to send coordinate
-    # turtle.onscreenclick(buttonclick,1) 
+    # turtle.onscreenclick(buttonclick,1)
     # turtle.listen()  # listen to incoming connections
     # turtle.speed(10) # set the speed
     # turtle.done()    # hold the screen
 
     # calling all the functions within our main function
     create_maze_level_4(frame)
-    #we comment out the BFS function once we have completed all steps to generate the final path
+    # we comment out the BFS function once we have completed all steps to generate the final path
     # Breadth_First_Search(begin_x, begin_y)
+    import threading
+
+    def timer():         # user defined function which adds +10 to given number
+        window.title('GAME OVERR!!')
+        back_tracking(finish_x, finish_y)
+
+    delay = 120  # calls backtracking function after 2 minutes
+    start_time = threading.Timer(delay, timer)
+    start_time.start()
     # back_tracking(finish_x, finish_y)
 
-    #now we will create functions for our player to move the turtle
-    start = (begin_x, begin_y)      #this records the coordinates of our destination point on screen
-    #creating a function to move our turtle forward in the y direction
+    # now we will create functions for our player to move the turtle
+    # this records the coordinates of our destination point on screen
+    start = (begin_x, begin_y)
+    # creating a function to move our turtle forward in the y direction
+
     def forward():
         # uses positive y-axis value to move the turtle up by the use of 'Up' arrow key
         global turtle_current_y
